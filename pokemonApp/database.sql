64908f892c605bd368c3efe46f613dacf2d9cdeb
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-10-13 19:25:22 +0200

    ultimo commit pokmeon

--- pokemonApp/database.sql
+++ pokemonApp/database.sql
@@ -8,7 +8,7 @@ create table pokemon (
   id bigint(20) not null auto_increment primary key,
   name varchar(100) not null unique,
   level int(3) not null,
-  evolution int(1) not null,
+  evolution int(1) not null
 ) engine=innodb default charset=utf8 collate=utf8_unicode_ci;
 
 create user pokemontrainer@localhost
