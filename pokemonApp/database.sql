4773e4905b799b275f7fa96d87b4bfc7ee5bb435
Daniel Fontalva Ostio <145746911+Danielfo684@users.noreply.github.com>
2024-10-18 12:55:05 +0200

    correccion pokemonApp

--- pokemonApp/database.sql
+++ pokemonApp/database.sql
@@ -8,6 +8,9 @@ create table pokemon (
   id bigint(20) not null auto_increment primary key,
   name varchar(100) not null unique,
   level int(3) not null,
+  weight float(5, 2) ,
+  height float(5, 2) ,
+  ptype varchar(100) ,
   evolution int(1) not null
 ) engine=innodb default charset=utf8 collate=utf8_unicode_ci;
 
